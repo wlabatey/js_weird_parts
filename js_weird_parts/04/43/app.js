422747b1d2d9f4e05af3f7475c2e4192e07a5562
Will Batey <will@willbatey.co.uk>
2016-04-28 18:04:47 +0100

    Finished L43

---
+++ js_weird_parts/04/43/app.js
@@ -0,0 +1,24 @@
+var 
+
+	// first name of the person
+	firstname,
+
+	// last name of the person
+	lastname, 
+
+	/*
+	   the language
+	   (can be 'en' or 'es')
+	*/
+	language;
+
+var person = {
+
+	// first name
+	firstname: 'John',
+
+	// lasat name
+	lastname: 'Doe'
+};
+
+console.log(person);
