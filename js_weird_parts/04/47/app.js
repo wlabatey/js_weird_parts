ddee8fb1bc3ff77038beb7b90a831e88be1d6d1d
Will Batey <will@willbatey.co.uk>
2016-04-30 12:17:36 +0100

    Progressed up to L48

--- js_weird_parts/04/47/app.js
+++ js_weird_parts/04/47/app.js
@@ -15,8 +15,39 @@ function buildFunctions() {
 
 var fs = buildFunctions();
 
+console.log(fs);
+
 fs[0]();
 fs[1]();
 fs[2]();
 
-console.log(arr);
+function buildFunctions2() {
+
+	var arr = [];
+
+	for (var i = 0; i < 3; i++) {
+
+		arr.push(
+
+			(function(j) {
+				return function() {
+					console.log(j);
+				};
+			}
+			(i)));
+	}
+
+	return arr;
+}
+
+var fs2 = buildFunctions2();
+
+// console.dir(fs); 
+console.log(fs2);
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
+
+
+
