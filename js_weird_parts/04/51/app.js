2cd05da3f558b1cbac08fc34afb596b5a839e373
Will Batey <will@willbatey.co.uk>
2016-05-05 16:58:24 +0100

    Finished 51

--- js_weird_parts/04/51/app.js
+++ js_weird_parts/04/51/app.js
@@ -32,5 +32,17 @@ var checkPastLimit = function(limiter, item) {
 	return item > limiter;
 };
 
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 2));
  console.log(arr4);
+
+ var checkPastLimitSimplified = function(limiter) {
+ 	return function(limiter, item) {
+ 		return item > limiter;
+ 	}.bind(this, limiter);
+ };
+
+var arr5 = mapForEach(arr1, checkPastLimitSimplified(2));
+console.log(arr5);
+
+// Do your best to make sure that your functions do not mutate data. 
+// Use new variables or arrays to store new data.
