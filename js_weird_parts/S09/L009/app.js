6bbfaab63eb91ad2b41ab4cd9f8d247d104eb3b9
Will Batey <will@willbatey.co.uk>
2016-09-25 20:44:28 +0100

    Completed S002/L009. The Global Environment & The Global Object.

---
+++ js_weird_parts/S09/L009/app.js
@@ -0,0 +1,21 @@
+/* The global object in the browser is the 'window' object. 
+
+ It can be different if javascript is being run on the server, like NodeJS.
+
+ 'this' is set to the global object by default.
+
+*/
+
+var a = "Hello world";
+
+function b() {
+
+
+
+}
+
+/* In Javascript, when you define variables outside of 
+
+    a function (in the global execution context) they get attached to the global object.
+
+*/
