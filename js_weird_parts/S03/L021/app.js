d03890b2199fbd1881f6ba33cca1b773e2c8b307
Will Batey <will@willbatey.co.uk>
2016-09-28 20:59:36 +0100

    Completed S03 L021 - Operators.

---
+++ js_weird_parts/S03/L021/app.js
@@ -0,0 +1,23 @@
+var a = 3 + 4;
+console.log(a);
+
+/*
+
+Operators are special functions that are syntactically (written) differently.
+
+Generally, operators take two parameters and return one result.
+
+Javascript, along with most programming languages, provides 'infix' notation, which allows us
+to use operators in between the two parameters we wish to work with.
+
+This means we can write 3 + 4 rather than having to do something like the below:
+
+function add(num1, num2) {
+	
+	return // add the two numbers
+
+}
+
+add(3, 4);
+
+*/
