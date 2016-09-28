995d8c21cec3a2c2174fb20dc03772dc420c8a08
Will Batey <will@willbatey.co.uk>
2016-09-28 21:24:22 +0100

    Completed S03 L022

---
+++ js_weird_parts/S03/L022/app.js
@@ -0,0 +1,15 @@
+var a = 2, b = 3, c = 4;
+
+a = b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+/*
+
+Operator precedence is which order operators are called in.
+
+Associativity is the directon that operators get called in, either left-toright or right-to-left.
+
+*/
