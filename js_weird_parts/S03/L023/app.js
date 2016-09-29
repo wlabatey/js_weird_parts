03da5fd2f9491438ece4eb5350d5f850f9a94c92
Will Batey <will@willbatey.co.uk>
2016-09-29 10:21:20 +0100

    Finished L023 - Coercion

---
+++ js_weird_parts/S03/L023/app.js
@@ -0,0 +1,13 @@
+var a = 1 + '2';
+console.log(a);
+
+/*
+
+The result of the above is 12. This is because Javascript coerced the number 1 into the string '1'
+and concatenated the two strings together to make '12'.
+
+Coercion is when Javascript attemps to 'coerce' or convert a value of one type to another.
+
+It happens quite often as JavaScript is a dynamically typed language.
+
+*/
