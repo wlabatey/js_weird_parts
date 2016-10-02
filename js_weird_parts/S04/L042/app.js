50576476da2da5f12904fa80de8d18719b2d7754
Will Batey <will@willbatey.co.uk>
2016-10-02 14:25:02 +0100

    Modified L042/app.js. Added comments.

--- js_weird_parts/S04/L042/app.js
+++ js_weird_parts/S04/L042/app.js
@@ -1,9 +1,11 @@
 function getPerson() {
 
-	return {
-		firstname: 'Tony'
-	};
+	return {				// It's important to note that, in javascript
+		firstname: 'Tony'	// if you do not input semicolins, it will try to automatically insert them.
+	
+	};						// This can cause unexpected problems, especially with return statements like this one.
 
-}
+}							// Always be sure to input all semicolons and never leave them out.
 
-console.log(getPerson());
+console.log(getPerson());	// And be very careful with return statements and using whitespace around them,
+							// as Javascript will automatically insert a semicolon if it sees a carriage return after a return statement.
