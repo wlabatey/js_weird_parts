0aad01b523796a517e9556bcd1937ad7cdcc227a
Will Batey <will@willbatey.co.uk>
2016-10-02 14:51:12 +0100

    Completed L043 - Whitespace

---
+++ js_weird_parts/S04/L043/app.js
@@ -0,0 +1,28 @@
+var 
+	// first name of the person
+	firstname, 
+	
+	// last name of the person
+	lastname, 
+	
+	// the language
+	// can be 'en' or 'es'	
+	language;
+
+var person = {
+	firstname: 'John',
+	lastname: 'Doe'	
+};
+
+console.log(person);
+
+/*
+
+Javascript is very liberal with whitespace, and you will frequently see comments like the above in 
+libraries and frameworks.
+
+This just helps yourself and other developers understand what the code is doing.
+
+Commenting your code well is a good habit to get into. Start now!
+
+*/
